d60df89c516f58fc76f4c6748ada782c0e03356c
Crazy Koodaa <crazykoodaa@vivaldi.net>
2022-03-31 12:10:03 +0200

    Anagram and Christmax Boxes Challenge finished

---
+++ Anagram/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualBasic;
+
+namespace AnagramImplementation
+{
+    public static class Anagram
+    {
+        public static Boolean IsAnagram(String firstWord, String secondWord)
+        {
+            // Generate list for the second word
+            List<char> vs2 = new List<char>();
+
+            // If the Length of both are different, then it's def. no anagram
+            if (firstWord.Length != secondWord.Length)
+                return false;
+            else
+            {
+                // Add all single chars from secondWord into 
+                // the second list
+                for (int i = 0; i < firstWord.Length; i++)
+                    vs2.Add(secondWord[i]);
+
+                // if the character in vs1 is in the second, 
+                // then remove it from the second. That should help, 
+                // if there are two same chars in the list
+                foreach (char c in firstWord)
+                    if (vs2.Contains(c)) vs2.Remove(c);
+
+            }
+
+            // if all chars are deleted in the second list, then it should be ok
+            if (vs2.Count == 0)
+                return true;
+            else
+                return false;
+        }
+    }
+}
---
+++ Christmas Boxes/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ChristmasBoxesImplementation
+{
+    public static class ChristmasBoxService
+    {
+        public static IEnumerable<ChristmasBox> GetShuffledPresentList(this List<ChristmasBox> boxList)
+        {
+            Comparer compare = new Comparer();
+            Console.WriteLine($"\n\n\nNumbers to check = {boxList.Count}");
+
+            // Copy List to a temp List
+            List<ChristmasBox> tempBox = boxList.ToList();
+
+            // Sort the Temp List descending
+            tempBox.Sort(compare);
+
+            // Rewrite the original List with the criteria bellow
+            for (int i = 0; i < boxList.Count; i++)
+            {
+                // If the rest of (i+1)%4 is 2, then it's the 2nd, or 5th, etc. position 
+                if ((i + 1) % 4 == 2)
+                {
+                    // take the biggest number which is in the tempBox on place 0 and delete 
+                    // the biggest number in temp. Therefore the 2nd biggest number will take
+                    // place 0;
+                    boxList[i] = tempBox[0];
+                    tempBox.RemoveAt(0);
+                }
+                else
+                {
+                    // take the smallest number from temp, and delete the smallest number from temp
+                    int countingTempBox = tempBox.Count - 1;
+                    boxList[i] = tempBox[countingTempBox];
+                    tempBox.RemoveAt(countingTempBox);
+                }
+            }
+            return boxList;
+        }
+    }
+    // just a class Comparer which takes the Interface Comparer to Sort the type ChristmasBox
+    class Comparer : IComparer<ChristmasBox>
+    {
+        // in a descending order
+        public int Compare(ChristmasBox x, ChristmasBox y)
+        {
+            if (x.Weight > y.Weight) return -1;
+            else if (x.Weight < y.Weight) return 1;
+            else return 0;
+        }
+    }
+}
